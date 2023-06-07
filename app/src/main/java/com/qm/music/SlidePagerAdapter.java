ed8b7aee013502b39fcacbd7131e43cb5f42cb06
zxf <3273703582@qq.com>
2023-06-07 17:42:28 +0800

    补充文件

---
+++ app/src/main/java/com/qm/music/SlidePagerAdapter.java
@@ -0,0 +1,48 @@
+package com.qm.music;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.qm.R;
+//适配器类
+public class SlidePagerAdapter extends PagerAdapter {
+
+    private Context context;
+    private int[] slideImages = {R.drawable.image1, R.drawable.image2, R.drawable.image3};
+
+    public SlidePagerAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return slideImages.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.slide_item, container, false);
+
+        ImageView imageView = view.findViewById(R.id.imageView);
+        imageView.setImageResource(slideImages[position]);
+
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+}
